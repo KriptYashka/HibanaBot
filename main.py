f4840431ad29ea8685fc9508ef71d4084202c64a
Егор Кучук <kriptyashka@gmail.com>
2023-02-08 22:28:04 +0300

    Init DB

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from discord.ext import commands
 
 from cmd import main_view, r6_view
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!')
 
 
 @bot.command()
